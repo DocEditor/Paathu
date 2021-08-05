27f3a32c5b38299f61e1bc6e29185600203f23de
DocEditor <88374079+DocEditor@users.noreply.github.com>
2021-08-05 14:53:46 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,19 +11,19 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/oBLYqk7.jpeg";
+  r_text[0] = "https://imgur.com/5wPp4Ta.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Fasil*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Doc*
 
-*owner number wa.me/918136831431?text=Hi*
-*paathu githublink   https://github.com/Fasil-vkn/Paathu*
+*owner number wa.me/919061363103?text=Hi*
+*bot number   wa.me/14243839432*
 
-*saidali liyamol githublink https://github.com/saidalisaid2/LiyaMol*
+*Made by DocEditormmx Bot name is Eva *
 
 `}) 
 
